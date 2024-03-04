74be4dfc594f35686e58fa2a4712c2256db24268
JuniorSierra <juniorsierramorales2002@gmailcom>
2024-03-04 13:54:28 -0500

    Agregué el ejercicio de frecuencia.

--- src/main/java/Ejercicios/Frecuencia.java
+++ src/main/java/Ejercicios/Frecuencia.java
@@ -1,5 +1,7 @@
 package Ejercicios;
 
+import java.util.HashMap;
+
 /**
  * Ejercicio de frecuencia de números generados aleatoriamente
  * @author Pablo José Hernández Meléndez [0221910052]
@@ -8,6 +10,31 @@ package Ejercicios;
  */
 public class Frecuencia {
     public static void run(){
-        System.out.println("Frecuencia de números");
+        int arreglo[] = new int[1000];
+
+        for (int i = 0; i < 1000; ++i) {
+            arreglo[i] = ((int) (Math.random() * 1000));
+        }
+
+        HashMap<Integer, Integer> frecuencia = new HashMap<>();
+        for (int i = 0; i < 1000; ++i) {
+            int num = arreglo[i];
+            frecuencia.put(num, frecuencia.getOrDefault(num, 0) + 1);
+        }
+
+        int elementoMasRepetido = -1;
+        int maxFrecuencia = 0;
+
+        for (int num : frecuencia.keySet()) {
+            int actualFrecuencia = frecuencia.get(num);
+            if (actualFrecuencia > maxFrecuencia) {
+                maxFrecuencia = actualFrecuencia;
+                elementoMasRepetido = num;
+            }
+        }
+
+        System.out.println("Ejercicio de Frecuencia");
+        System.out.println("El elemento que más se repite es: " + elementoMasRepetido);
+        System.out.println("Frecuencia de repetición: " + maxFrecuencia);
     }
 }
