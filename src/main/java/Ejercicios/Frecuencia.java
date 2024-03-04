4c01fb183c059cd044f48300ca1147ae41c22c4f
JuniorSierra <juniorsierramorales2002@gmailcom>
2024-03-04 15:24:33 -0500

    Quite hashmap para solo trabajar con arreglos.

--- src/main/java/Ejercicios/Frecuencia.java
+++ src/main/java/Ejercicios/Frecuencia.java
@@ -1,7 +1,5 @@
 package Ejercicios;
 
-import java.util.HashMap;
-
 /**
  * Ejercicio de frecuencia de números generados aleatoriamente
  * @author Pablo José Hernández Meléndez [0221910052]
@@ -9,27 +7,28 @@ import java.util.HashMap;
  * @author Sergio Alejandro Severiche Guerrero [0222010041]
  */
 public class Frecuencia {
-    public static void run(){
+    public static void run() {
         int arreglo[] = new int[1000];
 
         for (int i = 0; i < 1000; ++i) {
             arreglo[i] = ((int) (Math.random() * 1000));
         }
 
-        HashMap<Integer, Integer> frecuencia = new HashMap<>();
+        int[] frecuencia = new int[1001]; // Incrementamos el tamaño del arreglo para abarcar todos los posibles valores (0 a 1000)
+
         for (int i = 0; i < 1000; ++i) {
             int num = arreglo[i];
-            frecuencia.put(num, frecuencia.getOrDefault(num, 0) + 1);
+            frecuencia[num]++;
         }
 
         int elementoMasRepetido = -1;
         int maxFrecuencia = 0;
 
-        for (int num : frecuencia.keySet()) {
-            int actualFrecuencia = frecuencia.get(num);
+        for (int i = 0; i < frecuencia.length; i++) {
+            int actualFrecuencia = frecuencia[i];
             if (actualFrecuencia > maxFrecuencia) {
                 maxFrecuencia = actualFrecuencia;
-                elementoMasRepetido = num;
+                elementoMasRepetido = i;
             }
         }
 
